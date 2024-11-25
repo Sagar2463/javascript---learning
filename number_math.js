18257238f2b1e3ea795598fa5fc72e62884f72f4
sagar2463 <sagarkumar2463@gmail.com>
2024-11-26 01:27:58 +0530

    added num math filee

---
+++ number_math.js
@@ -0,0 +1,38 @@
+const { log } = require("console");
+
+const num1 = 24;
+const num2 = new Number(234.3445)
+
+// console.log(typeof(num2));// return object 
+// console.log(typeof(num1));// return number
+
+const score = 234.89;
+// console.log(score.toPrecision(),score.toString(),score.toFixed(1));// toFixed return the value after the point to only 1 234.8
+// console.log(num2.toPrecision());
+
+
+// ++++++++++++++++ math ++++++++++++++
+
+// console.log(Math.round(num2))//return 234
+// console.log(Math.abs(num2));// return 234.3445
+// console.log(Math.ceil(num2));// return 235
+// console.log(Math.floor(num2));// return234
+
+// question find the random number between max_value and min_value 
+// const min = 10;
+// const max = 30;
+//console.log(Math.floor((Math.random() * (max - min ))+1));
+const randomnum = ()=>{
+    const max = 30;
+    const min = 10;
+     return (Math.floor((Math.random() * (20)+1)+min))
+}
+//console.log(randomnum());// return random number between 10-30
+
+
+// +++++++++++ DATE +++++++++
+console.log(new Date(Date.now()).toLocaleString());
+
+
+
+
